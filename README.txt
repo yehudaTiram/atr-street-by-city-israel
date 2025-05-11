21ae44d7c68efe2fc4dc88c07252b9517ecc694a
Yehuda Tiram <yehuda@atarimtr.co.il>
2025-05-11 09:44:05 +0300

    Stable tag: 1.0.1

--- README.txt
+++ README.txt
@@ -6,7 +6,7 @@ Donate link: https://atarimtr.co.il/
 Tags: forms, city-street list, address
 Requires at least: 3.0.1
 Tested up to: 6.5.2
-Stable tag: 1.0.0
+Stable tag: 1.0.1
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
--- atr-street-by-city-israel.php
+++ atr-street-by-city-israel.php
@@ -39,7 +39,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'ATR_STREET_BY_CITY_ISRAEL_VERSION', '1.0.0' );
+define( 'ATR_STREET_BY_CITY_ISRAEL_VERSION', '1.0.1' );
 
 /**
  * The code that runs during plugin activation.
